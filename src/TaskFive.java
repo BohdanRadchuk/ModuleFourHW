cc04df217d013d6685da5f037894b7b02ccf799b
BohdanRadchuk <shpionuse@gmail.com>
2017-09-22 16:32:05 +0300

    5

--- src/TaskFive.java
+++ src/TaskFive.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TaskFive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите значение до которого нужно считать");
+        int a = scanner.nextInt();
+        int i = 0;
+        Count(a, i);
+    }
+
+    public static String Count (int a, int i) {
+
+        i++;
+        System.out.println(i);
+        if (i < a)
+            return Count(a, i);
+        return null;
+        }
+
+
 }
