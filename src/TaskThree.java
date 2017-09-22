84800756694430e96510ea349d9e7a75539e4541
BohdanRadchuk <shpionuse@gmail.com>
2017-09-22 22:05:13 +0300

    task3 +void

--- src/TaskThree.java
+++ src/TaskThree.java
@@ -1,15 +1,15 @@
 public class TaskThree {
-    static String drawRectangle(int l, int h) {     //rectangle
+    static void drawRectangle(int l, int h) {     //rectangle
         for (int j = 0; j < h; j++) {
             for (int i = 0; i < l; i++)
                 System.out.print("+");
             System.out.println();
         }
-            return null;
+
 
     }
 
-    static String drawRectangle(int l ) {           //overload square
+    static void  drawRectangle(int l ) {           //overload square
 
         for (int j = 0; j < l; j++) {
             for (int i = 0; i < l; i++)
@@ -17,11 +17,11 @@ public class TaskThree {
             System.out.println();
         }
 
-        return null;
+
     }
 
     public static void main(String[] args) {
-        drawRectangle (4,5);
+        drawRectangle (5, 4);
     }
 }
 
