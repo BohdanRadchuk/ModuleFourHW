114891c32fa6ad8b048bb4ed71b40d7159bb36bb
BohdanRadchuk <shpionuse@gmail.com>
2017-09-24 17:30:25 +0300

    task 6ready

--- src/TaskSix.java
+++ src/TaskSix.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class TaskSix {
+    public static String drawRectangle(int l, int h) {
+        if (l > 0) {
+            return drawRectangle(l - 1, h) + "+";
+        } else {
+            if (h > 1) {
+                return drawRectangle(6, h - 1) + "\n";
+            }
+        }
+        return "ваш прямогульник: \n";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(drawRectangle(6, 5));
+    }
 }
